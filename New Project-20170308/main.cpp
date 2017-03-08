3e7d2917703e133d7aa7b79774c7f5d7903d4a18
yasararafat123 <myasar3590@gmail.com>
2017-03-08 21:18:54 +0100

    Created by Coding Ground Client

---
+++ New Project-20170308/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int p1,p2;
+char game[10]={'n','a','b','c','d','e','f','g','h','i'};
+void grid(){
+    cout<< game[1]<<"||"<<game[2]<<"||"<<game[3]<< endl;
+    cout*<< game[4]<<"||"<<game[5]<<"||"<<game[6]<< endl;
+    cout<< game[7]<<"||"<<game[8]<<"||"<<game[9]<< endl << endl;
+    
+}
+
+
+
+int main()
+{
+    
+    grid(); //display  grid
+    //take input from users
+    While(){
+        if (game[p1] !='x'|| game [p1]= '0');
+        
+    }
+    
+    game[1]= 'x';
+    grid(); //updated grid display
+    
+    return 0;
+    
+   
+}
+
